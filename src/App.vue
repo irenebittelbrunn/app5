babba5551c0cec572c27da9801ab9ace7e024f1f
irenebittelbrunn <irenebittelbrunnp@gmail.com>
2026-03-17 11:56:08 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+const tarefas = ref([
+  { id: 1, desc:''}
+])
+</script>
+
+<template>
+<div class="container">
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
